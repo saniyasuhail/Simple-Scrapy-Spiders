134194e6c69393134f766a57350d91918be25f81
Saniya Suhail <suhailsaniya003@gmail.com>
2020-09-18 19:01:24 +0530

    simply scrapy spiders

---
+++ geekbuying/geekbuying/spiders/deals.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class DealsSpider(scrapy.Spider):
+    name = 'deals'
+    allowed_domains = ['www.geekbuying.com']
+    start_urls = ['https://www.geekbuying.com/deals']
+
+    def parse(self, response):
+        products = response.xpath("//div[@class='category_li']")
+        for product in products:
+            product_name = product.xpath(".//a[@class='category_li_link']/text()").get()
+            product_url = product.xpath(".//a[@class='category_li_link']/@href").get()
+            product_price = product.xpath(".//div[@class='category_li_price']/span/text()").get()
+            promotion_ends = product.xpath(".//div[@class='category_li_claibg']/span/text()").get()
+            yield {
+                'name': product_name,
+                'url': product_url,
+                'price': product_price,
+                'promotion': promotion_ends
+            }
+
+        next_page = response.xpath("//a[@class='next']/@href").get()
+        if next_page:
+            yield response.follow(url=next_page, callback=self.parse)
---
+++ worldometers/worldometers/spiders/countries.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from scrapy.shell import inspect_response
+from scrapy.utils.response import open_in_browser
+import logging
+
+
+class CountriesSpider(scrapy.Spider):
+
+    name = 'countries'
+    allowed_domains = ['www.worldometers.info']
+    start_urls = ['https://www.worldometers.info/world-population/population-by-country/']
+
+    def parse(self, response):
+       
+        #countries=response.xpath("//td/a")
+        #  for country in countries:
+        #          name=country.xpath(".//text()").get()  
+        #          link=country.xpath(".//@href").get() 
+
+        #          # yield{
+        #          #'country_name':name,
+        #          #'country_link':link
+        #           #}
+        #          #absolute_url=f"https://www.worldometers.info{link}"
+        #          absolute_url=response.urljoin(link) 
+                 #yield scrapy.Request(url=absolute_url)
+                 #yield response.follow(url=link, callback=self.parse_country,meta={'country_name':name})
+         yield response.follow(url="https://www.worldometers.info/world-population/china-population/", callback=self.parse_country,meta={'country_name':"China"})
+    def parse_country(self, response):
+        logging.info(response.status)
+        #logging.info(response.url)
+        # name=response.request.meta['country_name']
+        # rows=response.xpath("(//table[@class='table table-striped table-bordered table-hover table-condensed table-list'])[1]/tbody/tr")
+        # for row in rows:
+        #     year=row.xpath(".//td[1]/text()").get()
+        #     population=row.xpath(".//td[2]/strong/text()").get()
+
+        #     yield{
+        #         'country_name':name,
+        #         'year':year,
+        #         'population':population
+        #     }
+        #inspect_response(response,self)
+        open_in_browser(response)
